700634b8d2bf453e178d543912b514e15bb80bc6
Russ Hatcher <russell.hatcher@outlook.com>
2018-04-14 18:05:30 -0400

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=devenvexebotrnh-upgrade-Web-Deploy.pubxml -p:Password=NG9ExgbJTDM7nqCJNrTWgHpxv2FHNBoT9T6RCuN52nuQqwJortJc1him6yxN
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+# DevEnvExeBotRNH-src
